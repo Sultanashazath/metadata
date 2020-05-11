fbfac1b86a1aab232cfc40a4f8ce2a04879318a4
sultanashazath@gmail.com <sultanashazath@gmail.com>
2020-05-11 17:22:06 +0530

    files

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+const HOST = 'https://nftcards.herokuapp.com'
+
+module.exports = {
+  HOST
+}
---
+++ src/database.js
@@ -0,0 +1,58 @@
+module.exports = {
+  "1": {
+    "name": "NFT",
+    "birthday": "10-18"
+  },
+  "2": {
+    "name": "NFT",
+    "birthday": "01-08"
+  },
+  "3": {
+    "name": "NFT",
+    "birthday": "01-22"
+  },
+  "4": {
+    "name": "NFT",
+    "birthday": "03-04"
+  },
+  "5": {
+    "name": "NFT",
+    "birthday": "03-09"
+  },
+  "6": {
+    "name": "NFT",
+    "birthday": "03-09"
+  },
+  "7": {
+    "name": "NFT",
+    "birthday": "03-09"
+  },  
+  "8": {
+    "name": "NFT",
+    "birthday": "03-09"
+  }  ,
+  "9": {
+    "name": "NFT",
+    "birthday": "03-09"
+  },
+  "10": {
+    "name": "NFT",
+    "birthday": "03-09"
+  }
+  , "11": {
+    "name": "NFT",
+    "birthday": "03-09"
+  },
+  "12": {
+    "name": "NFT",
+    "birthday": "03-09"
+  },
+  "13": {
+    "name": "NFT",
+    "birthday": "03-09"
+  },
+  "14": {
+    "name": "NFT",
+    "birthday": "03-09"
+  }
+}
